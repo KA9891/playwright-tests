02269deb6155d00127fbb3c927e55210d9d65658
KA9891 <karthikeyanarora6@gmail.com>
2025-06-25 13:23:05 +0530

    new change2

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,7 +2,7 @@
 import { test, expect,chromium } from '@playwright/test';
 
 test('has title', async ({}) => {
-  const browser = await chromium.launch({headless : false})
+  const browser = await chromium.launch({headless : true})
   const context = await browser.newContext();
   const page = await context.newPage();
   await page.goto('https://playwright.dev/');
